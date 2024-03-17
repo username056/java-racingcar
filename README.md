a4e42ed00780367c8aee9e0b435a17ff74e949fd
kokodak <kokodakadokok@gmail.com>
2024-03-17 17:05:31 +0900

    feat: project init

---
+++ README.md
@@ -0,0 +1,3 @@
+# java-racingcar
+
+자동차 경주 미션 저장소
---
+++ src/main/java/RacingMain.java
@@ -0,0 +1,7 @@
+public class RacingMain {
+
+    public static void main(String[] args) {
+        // TODO: MVC 패턴을 기반으로 자동차 경주 미션 구현해보기
+        System.out.println("Hello, World!");
+    }
+}
